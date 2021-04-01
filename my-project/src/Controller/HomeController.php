4cbad4bce7ee95a4e3a2e8a3e84eba06ac0fa7cc
Mehdi <mehdi693@live.fr>
2021-04-01 12:55:22 +0200

    changement homePage

--- my-project/src/Controller/HomeController.php
+++ my-project/src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
