40b551da39701c7dd8d877752d2a9b76fb3bf1f9
Mehdi <mehdi693@live.fr>
2021-04-02 17:22:47 +0200

    FormRecruteur

--- my-project/src/Controller/RecruteurController.php
+++ my-project/src/Controller/RecruteurController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\RecruteurType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +13,13 @@ class RecruteurController extends AbstractController
     /**
      * @Route("/recruteur", name="recruteur")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $formulaire3 = $this->createForm(RecruteurType::class)->handleRequest($request);
+
         return $this->render('recruteur.html.twig', [
             'controller_name' => 'RecruteurController',
+            'formulaire3' => $formulaire3->createView()
         ]);
     }
 }
---
+++ my-project/src/Form/RecruteurType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recruteur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecruteurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Recruteur::class,
+        ]);
+    }
+}
