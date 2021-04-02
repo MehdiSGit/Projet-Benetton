b9f1dd7c227aaf5b896d527ff7cfe9a4a1f7f27d
Mehdi <mehdi693@live.fr>
2021-04-02 10:36:51 +0200

    Register / Sign in

--- my-project/src/Controller/AuthenticatorController.php
+++ my-project/src/Controller/AuthenticatorController.php
@@ -15,7 +15,7 @@ class AuthenticatorController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
+        //     return $this->redirectToRoute('ta_path');
         // }
 
         // get the login error if there is one
