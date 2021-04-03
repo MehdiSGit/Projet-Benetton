904ef924ffabd41b67739de1b2b04832d21fd9e9
Mehdi <mehdi693@live.fr>
2021-04-03 17:36:15 +0200

    update

--- my-project/src/Controller/RecruteurController.php
+++ my-project/src/Controller/RecruteurController.php
@@ -17,7 +17,7 @@ class RecruteurController extends AbstractController
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $recruteur = $this->getUser();
-        dd($recruteur);
+        // dd($recruteur);
         $formulaire3 = $this->createForm(RecruteurType::class)->handleRequest($request);
 
         if ($formulaire3->isSubmitted() && $formulaire3->isValid()){
