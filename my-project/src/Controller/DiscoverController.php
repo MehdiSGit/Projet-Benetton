d2bead7737975ba75d326b6ec1de13ca65eeac41
JacquesN <j.nguyen14216@gmai.com>
2021-04-07 17:38:11 +0200

    Make:controller Discover
    page pour des offres + barre de recherche

---
+++ my-project/src/Controller/DiscoverController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DiscoverController extends AbstractController
+{
+    #[Route('/lesoffres', name: 'offres')]
+    public function index(): Response
+    {
+        return $this->render('discover.html.twig', [
+            'controller_name' => 'DiscoverController',
+        ]);
+    }
+}
