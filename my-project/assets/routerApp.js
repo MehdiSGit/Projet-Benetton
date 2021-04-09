0822742ec2543fa307fa53494f1fc7d253168bde
JacquesN <j.nguyen14216@gmai.com>
2021-04-09 11:38:27 +0200

    Route React

---
+++ my-project/assets/routerApp.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Navlink,
+    BrowserRouter
+  } from "react-router-dom";
+
+  export default function App() {
+    <BrowserRouter>
+
+    <Navlink>
+    </Navlink>
+        <Route path="/home">
+
+        </Route>
+        <Route path="/candidat">
+
+        </Route >
+
+        <Route path="/recruteur">
+
+        </Route>
+        <Route>
+        </Route>
+    </BrowserRouter>}
