1777126f53732b78a1a2a21b0368db0e5cfd6b28
Mehdi <mehdi693@live.fr>
2021-04-09 14:36:05 +0200

    a

--- my-project/assets/components/Home.jsx
+++ my-project/assets/components/Home.jsx
@@ -4,21 +4,19 @@ import {Link} from "react-router-dom";
 
 class Home extends React.Component {
 
-  render() {
-    return (
-      <div>
+    render() {
+        return (
+            <div>
+                {/*<div className="alert alert-success">{{message}}</div>*/}
 
-        {/*<div className="alert alert-success">{{message}}</div>*/}
-
-        <Link to='/register'>Register</Link>
-        {/* Ne pas oublier de remplacer les a par des Link, et les href par des to*/}
-        <a>Sign in</a>
-        <a>Mon espace candidat</a>
-        <a>Mon espace recruteur</a>
-      </div>
-    )
-  }
-
+                <Link to='/register'>Register</Link>
+                {/* Ne pas oublier de remplacer les a par des Link, et les href par des to*/}
+                <a>Sign in</a>
+                <a>Mon espace candidat</a>
+                <a>Mon espace recruteur</a>
+            </div>
+        )
+    }
 }
 
 export default Home
--- my-project/assets/components/Register.jsx
+++ my-project/assets/components/Register.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 class Register extends React.Component {
 
-  render() {
-    return (
-      <div>
-        <h2>Composant register page</h2>
-      </div>
-    )
-  }
-
+    render() {
+        return (
+            <div>
+                <h2>Composant register page</h2>
+            </div>
+        )
+    }
 }
 
 export default Register
