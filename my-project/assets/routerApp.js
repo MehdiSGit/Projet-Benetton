d3b66f6bafa774626195687741cb6c2fd08a3335
JacquesN <j.nguyen14216@gmai.com>
2021-04-12 11:51:12 +0200

    Add Search component jsx

--- my-project/assets/routerApp.js
+++ my-project/assets/routerApp.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { BrowserRouter, NavLink, Route } from "react-router-dom";
-import Home from "./components/Home"
-import Register from "./components/Register"
 import CreateCandidate from "./components/CreateCandidate";
 
 export default function App() {
   return (
     <BrowserRouter>
 
-      <NavLink to='/home'>
-        Accueil
-      </NavLink>
 
       <NavLink to='/register'>
         Register
@@ -20,24 +15,13 @@ export default function App() {
         Create candidate
       </NavLink>
 
-      <Route path="/home">
-        <Home />
-      </Route>
-
-      <Route path="/register">
-        <Register />
-      </Route>
-
+      
 
       <Route path="/candidat">
       </Route>
 
       <Route path="/candidat">
       </Route >
-
-      <Route path="/register">
-        <Register></Register>
-      </Route>
 
       <Route path="/recruteur">
 
--- my-project/src/Controller/JobController.php
+++ my-project/src/Controller/JobController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Job;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\JobType;
@@ -22,13 +23,48 @@ class JobController extends AbstractController
     {
         $formulaireJob = $this->createForm(JobType::class);
         $jobs = $entityManager->getRepository(Job::class)->findBy([],['name'=>'DESC']);
+        //$jobs = $entityManager->getRepository(Job::class)->findAll();
 
 
+       
 
         return $this->render('job.html.twig', [
             'formulaireJob'   => $formulaireJob->createView(),
             'controller_name' => 'JobController',
             'jobs' => $jobs
         ]);
+    }
+
+    
+    /**
+     * @Route("/api/job", name="apijob")
+     */
+    public function fetchJob(HttpFoundationRequest $request, EntityManagerInterface $entityManager): Response
+    {
+        $jobs = $entityManager->getRepository(Job::class)->findAll();
+
+        $content = $request->getContent();
+        $jsonParameters = json_decode($content, true);
+        // On vérifie ici si on a des paramètres de recherche ou non, et sinon on fait le tri.
+        $hasParameters = isset($jsonParameters['search']) && $jsonParameters['search'];
+
+        if ($hasParameters) {
+            //for loop 
+            $search = $jsonParameters['search'];
+
+            $results = array_filter(
+                $jobs,
+                static function ($job) use ($search) {
+                   
+                
+                    return str_contains($job, $search);
+                });
+        }
+        else {
+            $results = $jobs;
+        }
+
+        return new JsonResponse($jobs);
+
     }
 }
