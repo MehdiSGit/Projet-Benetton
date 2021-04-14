ab066ca70a09745c60c1714eb1275237b996a5aa
Mehdi <mehdi693@live.fr>
2021-04-14 16:04:30 +0200

    update Bundle Reset Password

--- my-project/config/bundles.php
+++ my-project/config/bundles.php
@@ -18,4 +18,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
