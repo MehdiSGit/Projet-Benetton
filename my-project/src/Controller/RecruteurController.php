98d32a53e5da429e2aa11e4662d51ce6b05c4492
Mehdi <mehdi693@live.fr>
2021-04-14 11:04:53 +0200

    push recruteur

--- my-project/src/Controller/RecruteurController.php
+++ my-project/src/Controller/RecruteurController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-
 /** 
  *@IsGranted("ROLE_RECRUTEUR")
  */
@@ -23,7 +22,7 @@ class RecruteurController extends AbstractController
     {
         $recruteur = $this->getUser();
         // dd($recruteur);
-        $formulaire3 = $this->createForm(RecruteurType::class)->handleRequest($request);
+        $formulaire3 = $this->createForm(RecruteurType::class,$recruteur)->handleRequest($request);
 
         if ($formulaire3->isSubmitted() && $formulaire3->isValid()){
             // $formulaire3->getData() permet de récupérer l'objet Document (cf. Entity\Document.php)
