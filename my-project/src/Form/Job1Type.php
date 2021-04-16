64e9927f6952a4b4aea11887ec37d14351998544
Mehdi <mehdi693@live.fr>
2021-04-16 15:38:36 +0200

    Auto stash before merge of "master" and "origin/master"

--- my-project/src/Form/Job1Type.php
+++ my-project/src/Form/Job1Type.php
@@ -13,12 +13,30 @@ class Job1Type extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('code')
-            ->add('name')
-            ->add('description')
-            ->add('jobStartDate', DateType::class)
-            ->add('city')
-            ->add('TypeContrat')
+            ->add('code', null, [
+                'attr' =>['class' =>''],
+                'label_attr' =>['class' => 'label_code']
+            ])
+            ->add('name', null, [
+                'attr' =>['class' =>''],
+                'label_attr' =>['class' => 'label_name']
+            ])
+            ->add('description', null, [
+                'attr' =>['class' =>''],
+                'label_attr' =>['class' => 'label_description']
+            ])
+            ->add('jobStartDate', null, [
+                'attr' =>['class' =>''],
+                'label_attr' =>['class' => 'label_jobStartDate']
+            ], DateType::class)
+            ->add('city', null, [
+                'attr' =>['class' =>''],
+                'label_attr' =>['class' => 'label_city']
+            ])
+            ->add('TypeContrat', null, [
+                'attr' =>['class' =>''],
+                'label_attr' =>['class' => 'label_TypeContrat']
+            ])
         ;
     }
 
