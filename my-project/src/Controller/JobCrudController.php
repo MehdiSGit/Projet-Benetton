b536c7ccc9fc20ee708f2de1e2856d6e873764c9
JacquesN <j.nguyen14216@gmai.com>
2021-04-16 14:50:20 +0200

    edit details Job Crud

--- my-project/src/Controller/JobCrudController.php
+++ my-project/src/Controller/JobCrudController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/job/crud")
+ * @Route("/offres/modifier")
  */
 class JobCrudController extends AbstractController
 {
