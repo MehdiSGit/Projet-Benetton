4463bb4c5d1dea22caf57078bec4d9072d2b9a9d
Mehdi <mehdi693@live.fr>
2021-04-18 11:07:07 +0200

    correction route confirmationemail->login

--- my-project/src/Controller/RegistrationRecruterController.php
+++ my-project/src/Controller/RegistrationRecruterController.php
@@ -82,7 +82,7 @@ class RegistrationRecruterController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register_recruteur');
+        return $this->redirectToRoute('app_login');
     }
 
 
